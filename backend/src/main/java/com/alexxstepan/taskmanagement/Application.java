04dabd334fe995b52afd9dff256a08256d75b5c1
alexx.stepan <alexx.stepan@github.com>
2019-10-15 00:01:02 +0400

    Minor fix.

--- backend/src/main/java/com/alexxstepan/taskmanagement/Application.java
+++ backend/src/main/java/com/alexxstepan/taskmanagement/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class})
 //@EntityScan("com.alexxstepan.taskmanagement.entities")
 @EnableJpaRepositories(basePackages = {"com.alexxstepan.taskmanagement.dao.repositories"})
 @SpringBootApplication()
