9829f5ce815ef046f581b973f4289e20ac737c5f
alexx.stepan <alexx.stepan@github.com>
2019-11-04 17:15:08 +0400

    Authentication dialog.

--- backend/src/main/java/com/alexxstepan/taskmanagement/controllers/LoginController.java
+++ backend/src/main/java/com/alexxstepan/taskmanagement/controllers/LoginController.java
@@ -1,5 +1,8 @@
 package com.alexxstepan.taskmanagement.controllers;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
